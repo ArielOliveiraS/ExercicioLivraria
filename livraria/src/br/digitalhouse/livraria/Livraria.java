2c3329244d86d380f9e17622ef93a743c16c84d3
ArielOliveiraS <gildaariel@outlook.com>
2019-08-22 14:13:56 -0300

    subindo exercicio livraria

--- livraria/src/br/digitalhouse/livraria/Livraria.java
+++ livraria/src/br/digitalhouse/livraria/Livraria.java
@@ -5,11 +5,29 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Livraria {
-    public void comprar(){
+    private Livro novoLivro;
+    List<Livro> listaDeLivros = new ArrayList<>();
 
+    public void cadastrar(Livro novoLivro) {
+        listaDeLivros.add(novoLivro);
     }
-    public void consultar() {
 
+    public void consultar(Livro novoLivro) {
+        if (listaDeLivros.contains(novoLivro)) {
+            System.out.println(listaDeLivros);
+        } else {
+            System.out.println("Livro não encontrado");
+        }
+    }
 
+    public void efetuarVenda(Livro novoLivro) {
+        if (novoLivro.getQuantidadeEstoque() > 0) {
+            novoLivro.setQuantidadeEstoque(novoLivro.getQuantidadeEstoque()-1);
+            // listaDeLivros.remove(novoLivro);
+            System.out.println("Compra efetuada com sucesso!");
+        } else {
+            System.out.println("Estoque esgotado!");
+        }
     }
 }
+
--- livraria/src/br/digitalhouse/livraria/Main.java
+++ livraria/src/br/digitalhouse/livraria/Main.java
@@ -5,51 +5,35 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-
+        Livraria novaLivraria = new Livraria();
         Livro livro1 = new Livro(000, "Mulheres que correm com os lobos", "Clarissa Pinkola", 1992, 123, 2, 35);
         Livro livro2 = new Livro(001, "O Alquimista", "Paulo Coelho", 1990, 321, 7, 30);
         Livro livro3 = new Livro(002, "Pai rico, pai pobre", "Robert K", 1992, 213, 5, 40);
 
+        List<Livro> listaDeLivros = new ArrayList<>();
+
+        listaDeLivros.add(livro1);
+        listaDeLivros.add(livro2);
+        listaDeLivros.add(livro3);
+
         Scanner telaInicial = new Scanner(System.in);
-        System.out.println("O que você deseja : \n" + "1 para consultar \n" + "2 para comprar");
+        System.out.println("O que você deseja : \n" + "1 para consultar \n" + "2 para comprar \n" + "3 para cadastrar");
         int compraouconsulta = telaInicial.nextInt();
+        System.out.println(listaDeLivros.get(compraouconsulta));
 
         if (compraouconsulta == 1) {
-
-        } else {
-        }
-
-
-        List<Livro> listaDeLivros = new ArrayList<>();
-        try {
-            listaDeLivros.add(livro1);
-            listaDeLivros.add(livro2);
-            listaDeLivros.add(livro3);
-
-//            public void consultar(){
-//            for (Livro valor : listaDeLivros) {
-//                Scanner menu = new Scanner(System.in);
+//                Scanner telaConsulta = new Scanner(System.in);
 //                System.out.println("Qual livro você deseja consultar:  \n "+"0 para Mulheres que correm com os lobos \n"+" 1 para O alquimista \n" + " 2 para Pai rico, pai pobre");
-//                int escolha = menu.nextInt();
-//                System.out.println(listaDeLivros.get(escolha)); }
-//        } catch (Exception ex) {
-//            System.out.println("Livro não encontrado"); }}
-
-
-            for (Livro valor : listaDeLivros) {
-                Scanner menu = new Scanner(System.in);
-                System.out.println("Qual livro você deseja comprar: \n " + "0 para Mulheres que correm com os lobos \n" + " 1 para O alquimista \n" + " 2 para Pai rico, pai pobre");
-                int escolha = menu.nextInt();
-                System.out.println(listaDeLivros.get(escolha));
-                if (escolha > 0)
-                    listaDeLivros.remove(escolha);
-                System.out.println("Compra efetuada com sucesso!");
-            }
-
-        } catch (Exception ex) {
-            System.out.println("Estoque esgotado!");
+//                int escolha = telaConsulta.nextInt();
+          //      System.out.println(listaDeLivros.get(escolha));
+            novaLivraria.consultar(livro1);
+        } else if (compraouconsulta == 2) {
+            novaLivraria.efetuarVenda(livro2);
+        } else {
+            novaLivraria.cadastrar(livro3);
         }
-
 
     }
 }
+
+
